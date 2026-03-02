5343e1c817497193f6fca1e7596c9aeca67d6d8d
aibarrera2001 <aibarrera@unicesar.edu.co>
2026-03-02 13:18:06 -0500

    feat: agregar encapsulamiento a clase Asignatura

---
+++ src/main/java/Asignatura.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+/**
+ *
+ * @author Andrés
+ */
+public class Asignatura {
+     private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;
+}
