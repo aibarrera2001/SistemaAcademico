a598283d8aa875cf18495500b4585b0ed2fcbc19
aibarrera2001 <aibarrera@unicesar.edu.co>
2026-03-02 11:25:59 -0500

    feat: crear estructura base de Main con ArrayLists

--- src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
+++ src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
@@ -4,12 +4,20 @@
 
 package com.mycompany.sistemaacademico;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Andrés
  */
 public class SistemaAcademico {
-
+// ─── ArrayLists globales ───────────────────────────────────────────────────
+    static ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    static ArrayList<Asignatura> asignaturas = new ArrayList<>();
+    static ArrayList<Nota>       notas       = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+    static int contadorNotas = 1;
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
