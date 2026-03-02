32a372e40a467b432c8ef01793ab1333b71da7c9
aibarrera2001 <aibarrera@unicesar.edu.co>
2026-03-02 12:31:56 -0500

    feat:agregar encapsulamiento a clase Estudiante

--- src/main/java/Estudiante.java
+++ src/main/java/Estudiante.java
@@ -14,4 +14,24 @@ public class Estudiante {
     private String apellido;
     private String email;
     private String telefono;
+    
+    public Estudiante(String codigo, String nombre, String apellido, String email, String telefono) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getCodigo() { return codigo; }
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public String getEmail() { return email; }
+    public String getTelefono() { return telefono; }
+    
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+    public void setEmail(String email) { this.email = email; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
 }
