2ebdb68ae9277198d6ff256328d40a888b078a44
aibarrera2001 <aibarrera@unicesar.edu.co>
2026-03-02 12:29:14 -0500

    feat:crear clase Estudiante con atributos

---
+++ src/main/java/Estudiante.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+/**
+ *
+ * @author Andrés
+ */
+public class Estudiante {
+    
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+}
