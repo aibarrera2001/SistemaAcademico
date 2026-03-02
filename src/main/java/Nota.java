0bd9d964de173ce91234b03a0deca0bc78f5ec78
aibarrera2001 <aibarrera@unicesar.edu.co>
2026-03-02 13:24:56 -0500

    feat: agregar encapsulamiento a clase Nota

---
+++ src/main/java/Nota.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+/**
+ *
+ * @author Andrés
+ */
+public class Nota {
+    private int id;
+    private String codigoEstudiante;
+    private String codigoAsignatura;
+    private double nota1;
+    private double nota2;
+    private double nota3;
+    private double definitiva;
+}
